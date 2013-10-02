d15349e6726e1f78d7024bca443c9c3377c1b0bf
Vuk Stankovic <vuks89@gmail.com>
2013-10-02 21:28:25 +0200

    Added few tests

--- test/Movies/core_test.clj
+++ test/Movies/core_test.clj
@@ -1,7 +1,18 @@
 (ns Movies.core-test
-  (:use clojure.test
-        Movies.core))
+  (:use clojure.test)
+  (:require [Helpers.helpers :as h]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest movie-url-test
+    (is (= true (h/urlismovie "http://www.rottentomatoes.com/m/the_wolverine_2012/")))
+    (is (= false (h/urlismovie "http://www.rottentomatoes.com/m/the_wolverine_2012/")))
+    (is (= true (h/urlismovie "http://www.rottentomatoes.com/m/the_wolverine_2012/forum")))
+    (is (= false (h/urlismovie "http://www.rottentomatoes.com/m/the_wolverine_2012/forum"))))
+
+(deftest url-validation 
+  (is (= true (h/urlvalidator "http://www.rottentomatoes.com/m/the_wolverine_2012/")))
+  (is (= false (h/urlvalidator "http://www.rottentomatoes.com/m/the_wolverine_2012/")))
+  (is (= true (h/urlvalidator "http://www.rottentomatoes.com/celebrity/hugh_jackman/")))  
+  (is (= false (h/urlvalidator "http://www.rottentomatoes.com/celebrity/hugh_jackman/")))
+  )
+
+(run-tests)
